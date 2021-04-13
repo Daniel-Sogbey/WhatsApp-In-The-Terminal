1c7c6a6999b52a02dca96a468873e376c3b0b38c
Daniel Sogbey <mathematics06physics@gmail.com>
2021-04-13 23:11:36 +0000

    Update whatsapp.js

--- src/whatsapp.js
+++ src/whatsapp.js
@@ -5,20 +5,20 @@ venom.create().then(client => start(client)).catch(error => {
 });
 
 async function start(client) {
-	// client.onMessage(message => {
-	// if (message.body === "Hi" && message.isGroupMsg === false) {
-	// client
-	// .sendText(message.from, "Welcome Venom 🕷")
-	// .then(result => {
-	// console.log("Result: ", result); //return object success
-	// })
-	// .catch(error => {
-	// console.error("Error when sending: ", error); //return object error
-	// });
-	// }
-	// });
+	 client.onMessage(message => {
+	if (message.body === "Hi" && message.isGroupMsg === false) {
+	client
+	 .sendText(message.from, "Welcome Venom 🕷")
+	 .then(result => {
+	 console.log("Result: ", result); //return object success
+	 })
+	 .catch(error => {
+	console.error("Error when sending: ", error); //return object error
+	 });
+	}
+	});
 
-	const chats = await client.getAllChatsNewMsg();
+	// const chats = await client.getAllChatsNewMsg();
 
 	console.log(chats);
 }
